ea02f964c5a38794af1371aa4840c7e32dd918f0
FranciscoAbad <unlafranciscoabad@gmail.com>
2026-04-10 02:35:54 -0300

    chore: initial project scaffold with Docker setup for ophtra api

---
+++ ophtra/src/main/java/com/abdfs/ophtra/OphtraApplication.java
@@ -0,0 +1,13 @@
+package com.abdfs.ophtra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OphtraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OphtraApplication.class, args);
+	}
+
+}
